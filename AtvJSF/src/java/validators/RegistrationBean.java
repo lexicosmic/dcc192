9e393909c1f87a0f912d6e5f60b782cd98bb707e
Anastácia Malosto <anastacia.malosto@estudante.ufjf.br>
2023-11-14 22:52:09 -0300

    AtvJSF: Implementa app com Java Server Faces

---
+++ AtvJSF/src/java/validators/RegistrationBean.java
@@ -0,0 +1,55 @@
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class RegistrationBean {
+
+    private String salutation;
+    private String firstName;
+    private String lastName;
+    private Integer age;
+    private String email;
+
+//getters and setters omitted for brevity
+    public String getSalutation() {
+        return salutation;
+    }
+
+    public void setSalutation(String salutation) {
+        this.salutation = salutation;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
